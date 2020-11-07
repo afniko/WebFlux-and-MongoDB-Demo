7e57c79ac95ae5883c5b2460d6248ad0a4917ea0
afniko <afniko@gmail.com>
2020-11-07 09:13:44 +0200

    init(gradle.settings): init project #1

---
+++ flux-demo/src/main/java/com/afniko/FluxDemoBootApplication.java
@@ -0,0 +1,13 @@
+package com.afniko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxDemoBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxDemoBootApplication.class, args);
+    }
+
+}
