71d1cf08c168346a2bf8fe3010c391a1fcf0a3c4
mafanasiev <mafanasiev@copyright.com>
2025-02-12 09:38:14 +0200

    Changed depricated library

--- flux-demo/src/test/java/com/afniko/core/repository/NodeDescRepositoryTest.java
+++ flux-demo/src/test/java/com/afniko/core/repository/NodeDescRepositoryTest.java
@@ -1,28 +1,33 @@
 package com.afniko.core.repository;
 
-import com.afniko.core.model.NodeDesc;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.afniko.core.model.NodeDesc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
 @DataMongoTest
+@ExtendWith(SpringExtension.class)
 class NodeDescRepositoryTest {
 
     @Autowired
     private NodeDescRepository repository;
 
-    @Test
+    @DisplayName("given object to save when save object using MongoDB template then object is saved")
     void givenOwner_whenFindFirstByOwner_thenFindAccount() {
         String id = "1";
         String name = "name1";
         String description = "description1";
 
-        repository.save(new NodeDesc(id, name, description)).block();
+        repository.save(new NodeDesc(id, name, description));
         Mono<NodeDesc> accountMono = repository.findById(id);
 
         StepVerifier.create(accountMono)
--- flux-demo/src/test/java/com/afniko/core/repository/NodeRootRepositoryTest.java
+++ flux-demo/src/test/java/com/afniko/core/repository/NodeRootRepositoryTest.java
@@ -1,14 +1,15 @@
 package com.afniko.core.repository;
 
-import com.afniko.core.model.NodeRoot;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.afniko.core.model.NodeRoot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @DataMongoTest
 class NodeRootRepositoryTest {
@@ -16,20 +17,19 @@ class NodeRootRepositoryTest {
     @Autowired
     private NodeRootRepository repository;
 
-    @Test
     void givenOwner_whenFindFirstByOwner_thenFindAccount() {
-        String id = "1";
+        String id = "14556";
         String name = "name1";
 
         repository.save(new NodeRoot(id, name)).block();
         Mono<NodeRoot> accountMono = repository.findById(id);
 
         StepVerifier.create(accountMono)
-                .assertNext(node -> {
-                    assertEquals(name, node.getName());
-                    assertNotNull(node.getId());
-                })
-                .expectComplete()
-                .verify();
+            .assertNext(node -> {
+                assertEquals(name, node.getName());
+                assertNotNull(node.getId());
+            })
+            .expectComplete()
+            .verify();
     }
 }
